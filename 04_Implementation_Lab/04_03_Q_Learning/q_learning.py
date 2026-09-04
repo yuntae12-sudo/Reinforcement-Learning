766c9ed381a8a8b638b5b3c3fadbd175d7d8b975
yuntae12-sudo <agape0842@gmail.com>
2026-09-04 12:27:11 +0900

    Feat: ε-greedy 행동 선택 구현

--- 04_Implementation_Lab/04_03_Q_Learning/q_learning.py
+++ 04_Implementation_Lab/04_03_Q_Learning/q_learning.py
@@ -1,4 +1,5 @@
 from gridworld_env import GridWorldEnv
+import random
 
 env = GridWorldEnv()
 
@@ -10,6 +11,32 @@ for row in range(env.rows):
 
         q_table[state] = [0.0, 0.0, 0.0, 0.0]
 
+epsilon = 0.1
 
-for state, q_values in q_table.items():
-    print(state, q_values)
+def choose_action(state):
+    if random.random() < epsilon:
+        action = random.randint(0, 3)
+        print("Exploration:", action)
+
+    else:
+        q_values = q_table[state]
+
+        max_q = max(q_values)
+
+        best_actions = []
+
+        for action in range(4):
+            if q_values[action] == max_q:
+                best_actions.append(action)
+
+        action = random.choice(best_actions)
+
+        print("Exploitation:", action)
+
+    return action
+
+
+state = (0, 0)
+
+for _ in range(20):
+    choose_action(state)
